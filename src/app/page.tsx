6e5686df8b4749f52970133ed7d019f04e7b93d7
hanjiwon1108 <jwjwhan1108@gmail.com>
2026-02-08 11:25:57 +0900

    refactor: dynamics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Hero from "@/widgets/Hero/ui/Hero";
-import Services from "@/widgets/Services/ui/Services";
-import Showcase from "@/widgets/Showcase/ui/Showcase";
-import Testimonials from "@/widgets/Testimonials/ui/Testimonials";
-import Contact from "@/widgets/Contact/ui/Contact";
-import Footer from "@/widgets/Footer/ui/Footer";
+import dynamic from "next/dynamic";
+
+const Hero = dynamic(() => import("@/widgets/Hero/ui/Hero"), { ssr: true });
+const Services = dynamic(() => import("@/widgets/Services/ui/Services"), {
+  ssr: true,
+});
+const Showcase = dynamic(() => import("@/widgets/Showcase/ui/Showcase"), {
+  loading: () => (
+    <div className="h-screen w-full flex items-center justify-center">
+      Loading experiences...
+    </div>
+  ),
+});
+const Testimonials = dynamic(
+  () => import("@/widgets/Testimonials/ui/Testimonials"),
+  { ssr: true },
+);
+const Contact = dynamic(() => import("@/widgets/Contact/ui/Contact"), {
+  ssr: true,
+});
+const Footer = dynamic(() => import("@/widgets/Footer/ui/Footer"), {
+  ssr: true,
+});
 
 export default function Home() {
   return (
